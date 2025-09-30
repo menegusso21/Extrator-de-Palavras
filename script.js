ad1797c00ccda909183fb64b0b01f78037ab7fcf
TANIA MENEGUSSO <110191073+menegusso21@users.noreply.github.com>
2025-09-29 22:41:27 -0300

    .

--- script.js
+++ script.js
@@ -10,9 +10,21 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
+
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    const frenquencias = contaFrequencias(palavras); /////
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra); ///////////
 
+    function ordenaPalavra(p1, p2); {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas;Slice(0,10);
+}
+
+function contaFrequencias(palavras) {
     let frequencias = {};
     for (let i of palavras) {
         frequencias[i] = 0;
@@ -25,6 +37,5 @@ function processaTexto(texto) {
 
     console.log(frequencias);
 
-    return palavras;
 }
 
